960fb9b6769567a98ff1516a5dd8fde902514c85
Anand <anand.gupta@yantriks.com>
2018-05-09 19:33:30 -0400

    updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 import { AppComponent } from './app.component';
+import { environment } from './../environments/environment';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    CoursesListComponent,
+    AppFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+     apiKey: "AIzaSyDXn5ZyQhw5S2mV6v3krTrN3hggvcCat-E",
+    authDomain: "cryptowave-83f4b.firebaseapp.com",
+    databaseURL: "https://cryptowave-83f4b.firebaseio.com",
+    projectId: "cryptowave-83f4b",
+    storageBucket: "",
+    messagingSenderId: "694529326871"
+  }
 };
